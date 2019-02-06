476ac1fede7cb3a129ff9a1ab0b7d7a1c25e04ad
swetah3 <35355633+swetah3@users.noreply.github.com>
2019-02-06 10:33:30 +0530

    Create EVEN.java

---
+++ EVEN.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ int a;
+	Scanner input= new Scanner (System.in);
+//	System.out.println("the number is");
+	a=input.nextInt();
+	if(a%2==0)
+	{System.out.println("Even");}
+
+		// your code goes here
+
+		else{
+			System.out.println("Odd");}
+		}
+	}
