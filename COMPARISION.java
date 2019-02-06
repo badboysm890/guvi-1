4353560b23b74d9fbf4f09692418097595980708
swetah3 <35355633+swetah3@users.noreply.github.com>
2019-02-06 12:17:18 +0530

    Create COMPARISION.java

---
+++ COMPARISION.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int a,b,c;
+Scanner input=new Scanner (System.in);
+//System.out.println("input the numbers");
+a=input.nextInt();
+b=input.nextInt();
+c=input.nextInt();
+if((a>=b) && (a>=c))
+{
+	System.out.println(+a);
+	
+}
+else if((b>=a)&&(b>=c))
+{	System.out.println(+b);
+}else{
+System.out.println(+c);
+}
+}
+}
