b4e73b5ebc4c211dd8aaee393888aa309d74958d
swetah3 <35355633+swetah3@users.noreply.github.com>
2019-02-06 10:11:47 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ int a;
+	Scanner input= new Scanner (System.in);
+//.	System.out.println("the number is");
+	a=input.nextInt();
+	if(a>0)
+	{System.out.println("Positive");}
+	else if(a<0)
+	{
+	System.out.println("Negative");
+		// your code goes here
+	}
+		else{
+			System.out.println("Zero");}
+		}
+	}
